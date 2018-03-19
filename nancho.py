c8a7c4a83f11c4312e0953525f32669ec6103676
fazzeh <c.emmery@outlook.com>
2018-03-19 21:05:20 +0100

    added nancho

---
+++ nancho.py
@@ -0,0 +1,62 @@
+"""Mute music app if browser audio is playing."""
+
+# author: Chris Emmery
+# license: MIT
+
+import subprocess as sp
+import re
+from time import sleep
+
+
+def parse_pacmd(browser, music):
+    """Parses the pacmd list to find browser and music states."""
+    
+    states = {browser: 0, music: 0}
+    app_data = {}
+    
+    for line in sp.Popen("pacmd list-sink-inputs", shell=True, stdout=sp.PIPE,
+                         encoding='utf-8').stdout.read().split('\n'):
+        
+        if any(key + ': ' in line for key in ['index', 'state', 'client']):
+            line = re.sub(" {2,}|\t", '', line)
+            key, value = line.split(': ')
+            app_data[key] = value
+
+            if key == 'client':
+                for hook in states:
+                    if hook in app_data['client']:
+                        states[hook] = app_data['index'] if app_data['state'] == 'RUNNING' else 0
+                print("Found apps:\n", app_data)
+                app_data = {}
+
+    return states[browser], states[music]
+                 
+                 
+if __name__ == '__main__':
+    
+    import argparse
+    parser = argparse.ArgumentParser()
+    # data args
+    parser.add_argument('--browser', default='Firefox', help='The name identifier of your standard browser.')
+    parser.add_argument('--music', default='Spotify', help='The name identififier of your music app.')
+    parser.add_argument('--poll_time', type=int, default=3, help='Change the amount of time script parses pacmd.')
+    parser.add_argument('--pause', action='store_true', help='Pauses player rather than sending mute command (Spotify only!).')
+    args = parser.parse_args()
+
+    while True:
+
+        browser_state, music_state = parse_pacmd(args.browser, args.music)
+        pac_cmd = "pacmd set-sink-input-mute {ix} {val}"
+        spt_cmd = "dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.PlayPause"
+        # mute music
+        if browser_state and music_state:
+            print("... muting music!")
+            if not args.pause:
+                sp.Popen(pac_cmd.format(ix=music_state, val=1) if not args.pause else spt_cmd, shell=True)
+        # unmute music
+        if not browser_state and music_state:
+            print("... unumting music!")
+            if not args.pause:
+                sp.Popen(pac_cmd.format(ix=music_state, val=0) if not args.pause else spt_cmd, shell=True)
+
+        sleep(args.poll_time)
